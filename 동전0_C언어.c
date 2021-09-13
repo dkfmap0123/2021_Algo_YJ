9764e94492eb88946a3484b2325ca05408e8c0f3
dkfmap0123 <56014966+dkfmap0123@users.noreply.github.com>
2021-09-13 11:57:59 +0900

    Update 동전0_C언어.c
    2주차 / 일정 금액을 동전으로 환전 / C언어

--- 동전0_C언어.c
+++ 동전0_C언어.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int coin_num, value_num;
+    int coin_num, value_num; 
+	//coin_num:동전의 갯수, value_num:가격
     int count = 0;
-    int check = 0;
+  	//count:몇번 교환되는지 check
     
 	printf("동전갯수와 만들 가격을 정해주세요\n");
 	scanf("%d %d", &coin_num, &value_num);
@@ -13,6 +14,8 @@ int main() {
 	for(int i=0; i<coin_num; i++){
 	   scanf("%d", &coin[i]);
 	}
+	
+	//여기까지 동전갯수, 가격을 입력받고 동전갯수만큼 배열을 생성해 동전을 입력한다.
 	
 	for(int j=0; j<coin_num; j++){
 	    if(value_num<coin[j+1]){
@@ -26,5 +29,8 @@ int main() {
 	        continue;
 	    }
 	}
+	// 동전갯수만큼 반복문을 돌린다.
+	// 배열내 동전값이 value_num보다 커지면 바로 전 동전값을 value_num에서 빼고 count를 증가시킨다. 그 후 반복문을 처음부터 다시 돌도록 한다.
+	// count 계산이 끝나 value_num이 0이되면 count를 출력하고 반복문을 탈출한다.
 	return 0;
 }
