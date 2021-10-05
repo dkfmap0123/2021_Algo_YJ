340266430e3c5c968c37358df1d2f5daa6480707
dkfmap0123 <56014966+dkfmap0123@users.noreply.github.com>
2021-10-05 11:27:43 +0900

    시각 - python

---
+++ 5week/시각.py
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int h, cnt=0;
+int check(int h, int m, int s) {
+	if (h % 10== 3|| m / 10== 3|| m % 10== 3|| s / 10== 3|| s % 10== 3)
+		return 1;
+	return 0;
+}
+
+int main(void) {
+	scanf("%d",&h);
+	for(int i= 0; i<= h; i++) {
+		for(int j = 0; j < 60; j++) {
+			for(int k = 0; k < 60; k++) {
+				if(check(i, j, k)) cnt++;
+			}
+		}
+	}
+	printf("%d",cnt);
+	return 0;
+}
