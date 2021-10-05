4a43e256b5167e0c44d9ceab5b1a6b0b92919a49
dkfmap0123 <56014966+dkfmap0123@users.noreply.github.com>
2021-10-05 11:28:21 +0900

    시각 - c언어

--- 5week/시각.c
+++ 5week/시각.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 int h, cnt=0;
 int check(int h, int m, int s) {
 	if (h % 10== 3|| m / 10== 3|| m % 10== 3|| s / 10== 3|| s % 10== 3)
 		return 1;
+	
 	return 0;
 }
-
 int main(void) {
 	scanf("%d",&h);
+	
 	for(int i= 0; i<= h; i++) {
 		for(int j = 0; j < 60; j++) {
 			for(int k = 0; k < 60; k++) {
@@ -17,5 +17,6 @@ int main(void) {
 		}
 	}
 	printf("%d",cnt);
+	
 	return 0;
 }
